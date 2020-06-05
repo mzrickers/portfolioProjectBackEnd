0c7fc724c8bb08c48f3f87f671d8e0671f7653c4
Zach Rickers <mzrickers@gmail.com>
2020-06-05 07:55:36 -0600

    Express REST API with MongoDB and Mongoose Part 2

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -6,6 +6,8 @@ const gameRouter = express.Router();
 
 gameRouter.use(bodyParser.json());
 
+
+//Used to Create, Read, or Delete a game
 gameRouter.route('/')
 .get((req, res, next) => {
     Game.find()
@@ -40,6 +42,7 @@ gameRouter.route('/')
     .catch(err => next(err));
 });
 
+//Used to Read, Update, or Delete a specific game
 gameRouter.route('/:gameId')
 .get((req, res, next) => {
     Game.findById(req.params.gameId)
@@ -71,6 +74,149 @@ gameRouter.route('/:gameId')
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+// Used to Create, Read, or Delete comments from a specific game
+gameRouter.route('/:gameId/comments')
+.get((req, res, next) => {
+    Game.findById(req.params.gameId)
+    .then(game => {
+        if (game) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(game.comments);
+        } else {
+            err = new Error(`Game ${req.params.gameId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    Game.findById(req.params.gameId)
+    .then(game => {
+        if (game) {
+            game.comments.push(req.body);
+            game.save()
+            .then(game => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(game);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`Game ${req.params.gameId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /games/${req.params.gameId}/comments`);
+})
+.delete((req, res, next) => {
+    Game.findById(req.params.gameId)
+    .then(game => {
+        if (game) {
+            for (let i = (game.comments.length-1); i >= 0; i--) {
+                game.comments.id(game.comments[i]._id).remove();
+            }
+            game.save()
+            .then(game => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(game);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`Game ${req.params.gameId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
+// Used to Read, Update or Delete a specific comment
+gameRouter.route('/:gameId/comments/:commentId')
+.get((req, res, next) => {
+    Game.findById(req.params.gameId)
+    .then(game => {
+        if (game && game.comments.id(req.params.commentId)) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(game.comments.id(req.params.commentId));
+        } else if (!game) {
+            err = new Error(`Game ${req.params.gameId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /games/${req.params.gameId}/comments/${req.params.commentId}`);
+})
+.put((req, res, next) => {
+    Game.findById(req.params.gameId)
+    .then(game => {
+        if (game && game.comments.id(req.params.commentId)) {
+            if (req.body.rating) {
+                game.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.text) {
+                game.comments.id(req.params.commentId).text = req.body.text;
+            }
+            game.save()
+            .then(game => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(game);
+            })
+            .catch(err => next(err));
+        } else if (!game) {
+            err = new Error(`Game ${req.params.gameId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Game.findById(req.params.gameId)
+    .then(game => {
+        if (game && game.comments.id(req.params.commentId)) {
+            game.comments.id(req.params.commentId).remove();
+            game.save()
+            .then(game => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(game);
+            })
+            .catch(err => next(err));
+        } else if (!game) {
+            err = new Error(`Game ${req.params.gameId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
     })
     .catch(err => next(err));
 });
